dab561b7e03c41637e21a739f6ad8a1f5fc863bd
noaione <noaione0809@gmail.com>
2022-10-04 13:00:31 +0800

    frontend: add scripts

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,26 +1,25 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const usePlugins = [
-    "tailwindcss",
-    "postcss-flexbugs-fixes",
-    [
-        "postcss-preset-env",
-        {
-            autoprefixer: {
-                flexbox: "no-2009",
-            },
-            stage: 3,
-            features: {
-                "custom-properties": false,
-                "nesting-rules": true,
-            },
+    require("tailwindcss"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
+        autoprefixer: {
+            flexbox: "no-2009",
         },
-    ],
-    "autoprefixer",
+        stage: 3,
+        features: {
+            "custom-properties": false,
+            "nesting-rules": true,
+        },
+    }),
+    require("autoprefixer"),
 ];
 
 if (isProd) {
-    usePlugins.push(["cssnano", { preset: "default" }]);
+    usePlugins.push(require("cssnano")({
+        preset: "default"
+    }));
 }
 
 module.exports = {
