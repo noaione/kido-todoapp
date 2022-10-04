b554c72b0fa312bdc556a72f26a490d73fbd0dc4
noaione <noaione0809@gmail.com>
2022-10-04 13:01:23 +0800

    frontend: fix lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./styles/app.css";
 
 export default function App() {
-    return <div className="flex flex-col">Hello World!</div>
+    return <div className="flex flex-col">Hello World!</div>;
 }
